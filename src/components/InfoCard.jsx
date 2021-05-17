b255e5a8b3bd6528f38c47199e8212c73e5beb8a
Ash Z. Todd <ashztodd@gmail.com>
2021-05-16 17:48:08 -0700

    css rework & info cards

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const InfoCard = (props) => {
+
+    let { title, body, color } = props;
+
+    return (
+        <div className="info-card">
+            <div className="card-head">
+                <h3 style={{ color: color}}>{title}</h3>
+            </div>
+            <div className="card-body">
+                <p>{body}</p>
+            </div>
+        </div>
+    )
+};
+
+export default InfoCard;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ShinyButton from '../components/shiny-button';
 import Carousel from '../components/carousel';
 import Quizlet from '../components/quizlet';
+import Cards from '../components/cards';
 
 function Home(props) {
 
     return (
         <main>
-            <section className="flex">
+            <section className="bio">
                 <figure className="aside-left">
                     <div className="round-frame">
                         <img src="https://placekitten.com/g/200/200" alt="Sophia 'Charlie' Dever"/>
@@ -20,45 +21,18 @@ function Home(props) {
                     Whether your student is looking for help in a challenging math class, coaching for an upcoming test or contest, 
                     or to explore beyond the boundaries of what they're learning in school, we're here to help.</p>
 
-                    <p className="splash-text">At Teegarden Tutoring, we believe in:</p>
-
-                    <p className="splash-text">Student-led learning.</p>
-                    <p>An inquiry-based approach gives students genuine ownership of the math they learn, 
-                        and sets them up for math and the sciences in college and beyond. 
-                        We know that before you can find the right answers, you have to ask the right questions, 
-                        and we encourage students to pursue their questions about the math they learn in school: 
-                        why is that true? how do we know that? what's a counterexample?</p>
-
-                    <p className="splash-text">Setting students up for success.</p>
-                    <p>When a student struggles with math, it's usually because they haven't been handed the tools they need 
-                        to succeed. An intensive one-on-one approach allows us to tailor our instruction to the individual 
-                        needs of the student, in collaboration with their parents, their educational support team, 
-                        and the student themself. We have experience working with and advocating for autistic students, 
-                        LGBTQ+ students, and students with dyslexia, ADHD, anxiety, dyscalculia, and speech delays, 
-                        as well as profoundly gifted and twice-exceptional students.</p>
-
-                    <p className="splash-text">Results you can see.</p>
-                    <p>We ask our students to prove their results, so we owe it to them to do the same. 
-                        During our intake process, we set concrete goals; throughout the time we spend together, 
-                        we assess the student's learning at regular intervals and track their progress towards their goals. 
-                        If what we're doing isn't working, we need to know about it, so that we can figure out what to change.</p> 
-
-                    <p className="splash-text">The joy of math.</p>
-                    <p>We're in this business because we love math and we want to share that. If a student is miserable, 
-                        something has gone wrong. We work with students who struggle with math anxiety to rekindle 
-                        their love of learning and show them the fun side of math. When a student asks for 
-                        "just five more minutes", we know we've succeeded as teachers.</p>
-
-                    <p className="splash-text">If that sounds like a good fit for your student, click the button below to contact us for a free intake assessment and consultation!
-                    </p>
+                    {/* <p className="splash-text">If that sounds like a good fit for your student, click the button below to contact us for a free intake assessment and consultation!
+                    </p> */}
                     <ShinyButton />
                 </div>
             </section>
             <section className="carousel">
                 <Carousel />
             </section>
-            <section>
                 <Quizlet />
+            <section>
+                <h1 style={{ textAlign: 'center' }}>At Teegarden Tutoring, we believe in:</h1>
+                <Cards />
             </section>
         </main>
     )
