a0f44ca038c9564f09364b1ca9f85c82d2d3f9f6
Ash Z. Todd <ashztodd@gmail.com>
2021-05-16 18:25:58 -0700

    carousel improvements

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import Slider from "react-slick";
 
+const SLIDES = [
+    {
+        quote: `Alex left your session feeling very positive and said it was helpful.  
+            He thought you were very nice :) ... Grateful to have you on the team to support Alex.`,
+        attribution: "JoEllen",
+    },
+    {
+        quote: `[Aria] was very enthusiastic after her session today and proud of herself. 
+            She showed me how she worked through one of the problems below 
+            and was very excited about learning today as well.`,
+        attribution: "Arpita",
+    },
+    {
+        quote: `We were so pleased with [Student]'s performance on his most recent math test!`,
+    }
+]
+
+function Slide(props) {
+    const { quote, attribution } = props;
+
+    return (
+        <div className="slide">
+            <q className="quote">{quote}</q>
+            { attribution ? <div className="attribution">{attribution}</div> : "" }
+        </div>
+    )
+};
+
 function Carousel() {
     const settings = {
         dots: true,
@@ -13,15 +41,9 @@ function Carousel() {
     };
     return (
         <Slider {...settings}>
-            <div className="testimonial">
-                "Alex left your session feeling very positive and said it was helpful.  He thought you were very nice :) ... Grateful to have you on the team to support Alex." — JoEllen
-            </div>
-            <div className="testimonial">
-                "[Aria] was very enthusiastic after her session today and proud of herself. She showed me how she worked through one of the problems below and was very excited about learning today as well." — Arpita
-            </div>
-            <div className="testimonial">
-                "We were so pleased with [Student]'s performance on his most recent math test!"
-            </div>
+            {SLIDES.map((slide) => {
+                return <Slide {...slide} />
+            })}
         </Slider>
     )
 };
